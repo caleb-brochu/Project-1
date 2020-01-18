27e42dfe9bceb61fc49afa906dcff5538f469419
Joshua Lo <josh.lol1995@gmail.com>
2020-01-18 11:53:51 -0800

    added skeleton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,101 @@
 $( document ).ready(function() {
 
 })
+
+/**
+ * Function description
+ * Renders the User Interface
+ *
+ * @param - Takes no params
+ * @return - Does not return Anything
+ *
+ */
+function renderUI() {
+    // Top bar with location, start and end dates
+    // Summary
+    // Map
+    // Itinerary
+}
+
+/**
+ * Function description
+ * Gets information from the user (start and end date for trip and location)
+ *
+ * @param - Takes no params
+ * @return - Does not return Anything
+ *
+ */
+function topBar() {
+    // If 2 out of 3 of the input fields are filled in, have the page load once the 3rd is complete
+    
+    // Get input value from inputField
+    // Get input from calendar objects
+}
+
+
+/**
+ * Function description
+ * Dynamically appends (insert div) div to include an itinerary for the range of dates
+ * Suggests types of clothing based on weather, use switch case
+ *
+ * @param - Takes no params
+ * @return - Does not return anything
+ *
+ */
+function itinerary() {
+
+}
+
+
+/**
+ * Function description
+ * Takes in an object from weatherAPI
+ *
+ * @param - Takes in an object from weatherAPI.js that contains relevent weather data
+ * @return - returns weather info for a specific day
+ *
+ */
+function weather() {
+    // Go up to 7 day prediction
+    // After 7 days, predict weather using time machine data
+
+}
+
+/**
+ * Function description
+ * Append DOM element with the summary of the trip
+ * Total sum of all clothing, count pieces (shirts, pants)
+ *
+ * @param - Takes no params
+ * @return - Does not return Anything
+ *
+ */
+function summary() {
+
+}
+
+
+/**
+ * Function description
+ * 2 checkboxes that sees if the user wants to locate laundromats and/or stores
+ *
+ * @param - Takes no params
+ * @return - Does not return Anything
+ *
+ */
+function inputFields() {
+
+}
+
+
+/**
+ * Function description
+ * Displays the map
+ *
+ * @param - Location
+ * @return - Does not return Anything
+ *
+ */
+function displayMap() {
+
+}
